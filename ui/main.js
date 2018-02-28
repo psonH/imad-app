0138ef729de740d993f4cc0ec225c972ce9ea71a
psonH <35231175+psonH@users.noreply.github.com>
2018-02-28 12:33:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,10 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML="New Value";
+
+var img=document.getElemenetById('mardi');
+
+img.onclick= function()
+{
+    img.style.marginLeft='100px';
+};
