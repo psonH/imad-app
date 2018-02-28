638c2901244438f5af4241cde7462246a1e12ece
psonH <35231175+psonH@users.noreply.github.com>
2018-02-28 12:57:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,11 +10,11 @@ var img=document.getElementById('mardi');
 
 var marginLeft=0;
 function moveRight() {
-    var marginLeft=marginLeft+10;
+    var marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick= function()
 {
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
     
 };
