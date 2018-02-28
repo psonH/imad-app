b79bbede7545fbfe9fe8efa862652ee7ef5c6806
psonH <35231175+psonH@users.noreply.github.com>
2018-02-28 18:15:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,24 @@
 var button=document.getElementById('counter');
 
-var counter=0;
+
 button.onclick=function(){
+    //Create a request object
+    var request=new XMLHttpRequest();
     
-    
-    
-    
-    
-    
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+       if(request.readyState===XMLHttpRequest.DONE)
+       {
+          if(request.status === 200)
+          {
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+       }
+       //Not DONE yet
+     };
+     //Make request
+     request.open(GET, 'http://priyankhajela.imad.hasura-app.io/',true);
+     request.send(null);
 };
