a9e2a4f469f45bd95ff78786cb2433d248de1399
psonH <35231175+psonH@users.noreply.github.com>
2018-03-04 23:45:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,22 @@ button.onclick=function(){
      request.open('GET','http://priyankhajela.imad.hasura-app.io/counter',true);
      request.send(null);
 };
+
+
+//Submit name
+ var nameInput=document.getElementById('name');
+ var naam=nameInput.value;
+ var submit=document.getElementById('submit_btn');
+ onclick.submit=function(){
+     //
+     var names=['name1','name2','name3','name4'];
+     var list='';
+     for(var i=0;i<names.length;i++)
+     {
+         list +='<li>'+names[i]+'</li>';
+     }
+     var ul=document.getElementById('namelist');
+     ul.innerHTML=list;
+ };
+ 
+ 
