ae56b8fd576f15d9f298a41bc8efe91e9e7ccccd
psonH <35231175+psonH@users.noreply.github.com>
2018-03-05 00:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,15 +29,30 @@ button.onclick=function(){
  var naam=nameInput.value;
  var submit=document.getElementById('submit_btn');
  submit.onclick=function(){
-     //
-     var names=['name1','name2','name3','name4'];
-     var list='';
-     for(var i=0;i<names.length;i++)
-     {
-         list +='<li>'+names[i]+'</li>';
-     }
-     var ul=document.getElementById('namelist');
-     ul.innerHTML=list;
+     
+    var request=new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.DONE)
+       {
+          if(request.status === 200)
+          {
+             var names=request.responseText;
+             names=JSON.parse(names);
+             var list='';
+             for(var i=0;i<names.length;i++)
+             {
+                 list +='<li>'+names[i]+'</li>';
+             }
+             var ul=document.getElementById('namelist');
+             ul.innerHTML=list;
+          }
+       }
+       //Not DONE yet
+     };
+     //Make request
+     request.open('GET','http://priyankhajela.imad.hasura-app.io/submit-name?name='+naam,true);
+     request.send(null);
  };
  
  
